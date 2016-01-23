a08e361afbdf356b14f8894f166e3e9342451677
Brad Taylor <bradtaylorsf@gmail.com>
2016-01-23 00:28:02 -0800

    new popup code

--- popup.js
+++ popup.js
@@ -1,14 +1,35 @@
 $(document).ready(function(){
+
     $("#cpa_stalkee").submit(function(event){
         event.preventDefault();
-
         var stalkee = $("#cpa_text").val();
-        chrome.storage.local.set({'cpa_stalkee': stalkee}, function (result) {
-            window.close();
+        chrome.storage.local.set({'cpa_stalkee': stalkee,'cpa_time': new Date()}, function (result) {
+            $('#cpa_stalkee_un').text(stalkee);
+            $('.active-stalker').show();
+            $('.add-stalker').hide();
         });
     });
 
     chrome.storage.local.get('cpa_stalkee', function (result) {
+      if(result.cpa_stalkee && result.cpa_stalkee != ''){
+        $('.add-stalker').hide();
+        $('#cpa_stalkee_un').text(result.cpa_stalkee);
+        $('.active-stalker').show();
+        $('#countdown').countup();
+      }
         console.log( result );
     });
+
+    function clearUser(){
+      chrome.storage.local.set({'cpa_stalkee': ''});
+    }
+
+    $('.over-it').on('click',function(){
+      chrome.storage.local.set({'cpa_stalkee': ''},function(){
+        $('.active-stalker').hide();
+        $('.add-stalker').show();
+      });
+    });
+
+
 });
